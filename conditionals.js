23f580b2d2854325da440cfa890fca6938a52d28
Vivian Zhang <vvynz12@gmail.com>
2021-12-25 00:38:08 +0000

    fixing things

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+const temperature = -5;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+const raining = true;
+if (!raining) {
+  console.log("leave youre umbrella at home!");
+
