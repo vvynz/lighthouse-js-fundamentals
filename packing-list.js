797d25d536650c186d32f1baaada79b188f82edc
Vivian Zhang <vvynz12@gmail.com>
2021-12-26 22:46:27 +0000

    changed the for loop to a while loop

--- packing-list.js
+++ packing-list.js
@@ -10,6 +10,9 @@ let packingList = [
 ];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
